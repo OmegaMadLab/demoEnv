a809f4c30044fdfbab00d73a215c11f986d059b9
OmegaMadLab <locurtola@tin.it>
2017-05-23 16:11:38 +0200

    First commit

---
+++ sidit17/scripts/CreateADDC.ps1/CreateADDC.ps1
@@ -0,0 +1,90 @@
+﻿configuration CreateADDC 
+{ 
+   param 
+   ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    ) 
+    
+    Import-DscResource -ModuleName xActiveDirectory, xNetworking, PSDesiredStateConfiguration
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+
+    Node localhost
+    {
+		Script script1
+		{
+      	    SetScript =  { 
+				Set-DnsServerDiagnostics -All $true
+                Write-Verbose -Verbose "Enabling DNS client diagnostics" 
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+			DependsOn = "[WindowsFeature]DNS"
+        }
+	
+		WindowsFeature DNS 
+        { 
+            Ensure = "Present" 
+            Name = "DNS"		
+        }
+
+		WindowsFeature DnsTools
+		{
+			Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+		}
+
+        xDnsServerAddress DnsServerAddress 
+        { 
+            Address        = '127.0.0.1' 
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+			DependsOn = "[WindowsFeature]DNS"
+        }
+
+        WindowsFeature ADDSInstall 
+        { 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+        }  
+
+		WindowsFeature ADAdminCenter 
+        { 
+            Ensure = "Present" 
+            Name = "RSAT-AD-AdminCenter"
+			DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+		
+		WindowsFeature ADDSTools 
+        { 
+            Ensure = "Present" 
+            Name = "RSAT-ADDS-Tools"
+			DependsOn = "[WindowsFeature]ADDSInstall"
+        }  
+
+        xADDomain FirstDS 
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "C:\NTDS"
+            LogPath = "C:\NTDS"
+            SysvolPath = "C:\SYSVOL"
+			DependsOn = "[WindowsFeature]ADDSInstall"
+        } 
+
+        LocalConfigurationManager 
+        {
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+   }
+} 
